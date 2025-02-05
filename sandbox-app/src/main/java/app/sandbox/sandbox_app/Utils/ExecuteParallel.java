fd13f1d1dc3a0300b131f1a92f6994eefb23d72e
S-mishina <45090872+S-mishina@users.noreply.github.com>
2025-02-05 19:36:01 +0900

    fix
    Signed-off-by: S-mishina <45090872+S-mishina@users.noreply.github.com>

--- sandbox-app/src/main/java/app/sandbox/sandbox_app/Utils/ExecuteParallel.java
+++ sandbox-app/src/main/java/app/sandbox/sandbox_app/Utils/ExecuteParallel.java
@@ -2,15 +2,19 @@ package app.sandbox.sandbox_app.Util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@Component
 public class ExecuteParallel {
+
+  @Autowired
   private ThreadPoolTaskExecutor taskExecutor;
 
-  public static List<String> ExecuteParallelHttpRequest(int parallelCount) {
+  public List<String> executeParallelHttpRequest(int parallelCount) {
     String url = System.getenv("TEST_ENDPOINT");
     if (url == null) {
       return List.of("TEST_ENDPOINT environment variable is not set");
